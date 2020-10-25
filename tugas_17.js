a5f8d4534bd3c1fcf8657ba9ff6d06ad587ce372
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-25 23:39:34 +0700

    Add files via upload

---
+++ tugas_17.js
@@ -0,0 +1,10 @@
+function Ubah(){
+  var kalimat1 ="Saya beLajar bahaSa peMrograman deNgan khUsuk"
+  var kalimat2 ="Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual"
+console.log(kalimat1.toLowerCase());
+console.log(kalimat1.toUpperCase());
+console.log(kalimat2.toLowerCase());
+console.log(kalimat2.toUpperCase());
+}
+
+Ubah();
